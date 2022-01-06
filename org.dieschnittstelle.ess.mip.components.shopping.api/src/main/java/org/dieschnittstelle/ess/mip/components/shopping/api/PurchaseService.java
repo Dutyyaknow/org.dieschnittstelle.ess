859cb1d0e8c9390f5dc4b820050c9eff9d05a710
Oliver <s75583@beuth-hochschule.de>
2022-01-06 10:57:32 +0100

    PAT 1 finished

--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -1,8 +1,17 @@
 package org.dieschnittstelle.ess.mip.components.shopping.api;
 
 // TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/purchase")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface PurchaseService {
 
-	void purchaseCartAtTouchpointForCustomer(long shoppingCartId, long touchpointId, long customerId) throws ShoppingException;
+	@POST
+	@Path("/{shoppingCartId}/{touchpointId}/{customerId}")
+	void purchaseCartAtTouchpointForCustomer(@PathParam("shoppingCartId") long shoppingCartId, @PathParam("touchpointId") long touchpointId, @PathParam("customerId") long customerId) throws ShoppingException;
 	
 }
